f8ad252d390a7ce6057989e5227bf17bed92a60c
Douglas Germano <dogermano@gmail.com>
2011-04-21 12:38:31 -0300

    Baixando as fotos com threads e batch request.

--- teste.py
+++ teste.py
@@ -1,6 +1,9 @@
 import json
 import pickle
+import urllib
 import urllib2
+import threading
+import time
 
 graph = 'https://graph.facebook.com/'
 access_token = # Get access token the hacky way by going to 
@@ -40,17 +43,45 @@ def get_users_groups(user_ids):
   return get_users_connections(user_ids, get_user_groups)
 
 def save_user_picture(user = 'ceroma', size = 'large'):
-  # Request profile picture:
-  pic = urllib2.urlopen(graph + user + '/picture?type=' + size)
-
-  # Save picture:
-  f = open(user + '.jpg', 'w')
-  f.write(pic.read())
+  try:
+    # Request profile picture:
+    pic = urllib2.urlopen(graph + user + '/picture?type=' + size)
+  
+    # Save picture:
+    f = open(user + '.jpg', 'w')
+    f.write(pic.read())
+  except:
+    save_user_picture(user)
 
 def save_users_pictures(user_ids, size = 'large'):
   for uid in user_ids:
-    save_user_picture(uid, size)
+    args = (uid, )
+    if threading.activeCount() > 200:
+      time.sleep(1)
+    threading.Thread(target = save_user_picture, args = args).start()
 
-fofs = get_users_friends(get_user_friends())
-print len(fofs)
+def batch_request(user_ids):
+  response = []
+  request = []
+  for uid in user_ids:
+    request.append({"method": "GET", "relative_url": uid + '/friends'})
+
+    if len(request) == 20:
+      args = {}
+      args['access_token'] = access_token
+      args['batch'] = json.dumps(request)
+      data = json.loads(urllib2.urlopen(graph, urllib.urlencode(args)).read())
+      for user in data:
+        if user['code'] == 200:
+          for friendId in json.loads(user['body'])['data']:
+            response.append(friendId['id'])
+      request = []
+        
+  return response
+  
+friends = get_user_friends()
+fofs = batch_request(friends)
+save_users_pictures(fofs)
+while threading.activeCount() > 1:
+  time.sleep(1)
 pickle.dump(fofs, open("fofs.pck", "w"))
