8dfab9fde86558997e7844b480f56069e1b6c6b3
Douglas Germano <dogermano@gmail.com>
2011-04-21 15:34:34 -0300

    Cleaning the batch request.

--- teste.py
+++ teste.py
@@ -6,8 +6,8 @@ import threading
 import time
 
 graph = 'https://graph.facebook.com/'
-access_token = # Get access token the hacky way by going to 
-               # http://developers.facebook.com/docs/reference/api/ 
+access_token = '2227470867|2.ijPpUMki73Ffx2MsKU_16w__.3600.1303412400.0-647275296|5ZeIJS9qmF8KjYGXYm9c23ip2oI'
+max_retry = 200
 
 def get_user_connections(user = 'me', type = 'friends'):
   # Request connections:
@@ -26,23 +26,7 @@ def get_user_connections(user = 'me', type = 'friends'):
 def get_user_friends(user = 'me'):
   return get_user_connections(user, type = 'friends')
 
-def get_user_groups(user = 'me'):
-  return get_user_connections(user, type = 'groups')
-
-def get_users_connections(user_ids, connection_getter):
-  ids = {}
-  for user in user_ids:
-    for id in connection_getter(user):
-      ids[id] = id
-  return ids
-
-def get_users_friends(user_ids):
-  return get_users_connections(user_ids, get_user_friends)
-
-def get_users_groups(user_ids):
-  return get_users_connections(user_ids, get_user_groups)
-
-def save_user_picture(user = 'ceroma', size = 'large'):
+def save_user_picture(user = 'ceroma', size = 'large', timeout = 10):
   try:
     # Request profile picture:
     pic = urllib2.urlopen(graph + user + '/picture?type=' + size)
@@ -51,37 +35,38 @@ def save_user_picture(user = 'ceroma', size = 'large'):
     f = open(user + '.jpg', 'w')
     f.write(pic.read())
   except:
-    save_user_picture(user)
+    timeout -= 1
+    print "Timeout %d para user %s" %(timeout, user)
+    save_user_picture(user, timeout = timeout)
 
 def save_users_pictures(user_ids, size = 'large'):
   for uid in user_ids:
-    args = (uid, )
-    if threading.activeCount() > 200:
+    while threading.activeCount() > max_retry:
       time.sleep(1)
-    threading.Thread(target = save_user_picture, args = args).start()
+    threading.Thread(target = save_user_picture, args = (uid, )).start()
 
-def batch_request(user_ids):
-  response = []
-  request = []
-  for uid in user_ids:
-    request.append({"method": "GET", "relative_url": uid + '/friends'})
+def batch_request(request):
+  args = {'access_token' : access_token, 'batch' : json.dumps(request)}
+  return json.loads(urllib2.urlopen(graph, urllib.urlencode(args)).read())
 
-    if len(request) == 20:
-      args = {}
-      args['access_token'] = access_token
-      args['batch'] = json.dumps(request)
-      data = json.loads(urllib2.urlopen(graph, urllib.urlencode(args)).read())
-      for user in data:
-        if user['code'] == 200:
-          for friendId in json.loads(user['body'])['data']:
-            response.append(friendId['id'])
-      request = []
-        
+def get_users_friends(user_ids):
+  response = {}
+  i = 0
+  while (i < len(user_ids)):
+    request = [{"method": "GET", "relative_url": uid + '/friends'} for uid in user_ids[i:i+20]]
+    data = batch_request(request)
+    for user in data:
+      if user['code'] == 200:
+        for friend in json.loads(user['body'])['data']:
+          response[friend['id']] = friend['id']
+    i = i + 20
+
   return response
   
 friends = get_user_friends()
-fofs = batch_request(friends)
-save_users_pictures(fofs)
+fofs = get_users_friends(friends)
+save_users_pictures(fofs.keys())
 while threading.activeCount() > 1:
+  print threading.activeCount()
   time.sleep(1)
 pickle.dump(fofs, open("fofs.pck", "w"))
